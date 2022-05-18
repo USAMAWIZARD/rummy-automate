dc53d8a9ac5aee9766490820a9741b2536f51a3e
Sunil Thakur <sunilthakur178co@gmail.com>
2022-05-18 08:42:06 +0530

    added deck logic to sequence

--- constants.py
+++ constants.py
@@ -2,14 +2,17 @@ ranks = {'joker': 0, 'ace': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '
 
 suits = ['spades', 'hearts', 'diamonds', 'clubs']
 
-# hand_cards = [[['8', 'diamonds'], ['10', 'diamonds']], [['4', 'clubs'], ['king', 'clubs'], ['ace1', 'clubs']], [['8', 'hearts'], ['10', 'hearts'], ['5', 'hearts'], ['ace', 'hearts']], [['joker', 'spades'], ['8', 'spades'], ['queen', 'spades']], [['joker', None]]]
+# deck = ['queen', 'clubs']
+deck = ['7', 'hearts']
+
+hand_cards = [[['8', 'diamonds'], ['10', 'diamonds']], [['4', 'clubs'], ['king', 'clubs'], ['ace1', 'clubs']], [['8', 'hearts'], ['10', 'hearts'], ['5', 'hearts'], ['ace', 'hearts']], [['joker', 'spades'], ['8', 'spades'], ['queen', 'spades']], [['joker', None]]]
 # hand_cards = [[['2', 'clubs'], ['3', 'clubs'], ['4', 'clubs'], ['7', 'clubs'], ['8', 'clubs'], ['jack', 'clubs'], ['king', 'clubs'], ['ace1', 'clubs']]]
 
 # hand_cards = [[['2', 'clubs'], ['3', 'clubs'], ['4', 'clubs'], ['7', 'clubs'], ['8', 'clubs'], ['jack', 'clubs'], ['king', 'clubs'], ['ace1', 'clubs']]]
 
 # hand_cards = [[['2', 'clubs'], ['3', 'clubs'], ['6', 'clubs'], ['8', 'clubs'], ['9', 'clubs'], ['jack', 'clubs'], ['king', 'clubs'], ['ace1', 'clubs']]]
 
-hand_cards = [[['king', 'clubs'], ['ace', 'clubs']]]
+# hand_cards = [[['king', 'clubs'], ['ace', 'clubs']]]
 
 # hand_cards = [[['4', 'clubs'], ['king', 'clubs'], ['ace1', 'clubs']]]
 
--- sequence.py
+++ sequence.py
@@ -1,127 +1,181 @@
-from itertools import count
+from operator import le
 from constants import *
 from check import *
 from copy import deepcopy
 
-def findJoker(cards, suit = True):
-	i = 0
-	while i < len(cards):
-		if isJoker(cards[i]):
-			return jokerType(cards[i]) == suit
-		i += 1
-	return False
+
+def findJoker(cards, suit=True):
+    i = 0
+    while i < len(cards):
+        if isJoker(cards[i]):
+            return jokerType(cards[i]) == suit
+        i += 1
+    return False
+
 
 jokers = []
 new_cards = []
 
+
 def getJoker(suit):
-	global jokers
-	if ["joker", suit] in jokers:
-		jokers.remove(["joker", suit])
-		return ["joker", suit]
-	elif ["joker", None] in jokers:
-		jokers.remove(["joker", None])
-		return ["joker", None]
-	elif jokers != []:
-		return jokers[0]
-	return False
+    global jokers
+    if ["joker", suit] in jokers:
+        jokers.remove(["joker", suit])
+        return ["joker", suit]
+    elif ["joker", None] in jokers:
+        jokers.remove(["joker", None])
+        return ["joker", None]
+    elif jokers != []:
+        joker = jokers[0]
+        del jokers[0]
+        return joker
+    return False
+
 
 def checkIfCardRankExists(cards, rank):
-	i = 0
-	while i < len(cards):
-		if ranks[cards[i][0]] == rank:
-			return True
-		i += 1
-	return False
+    i = 0
+    while i < len(cards):
+        if ranks[cards[i][0]] == rank:
+            return True
+        i += 1
+    return False
+
+
+def checkIfSuitSame(p_cards, suit):
+    i = 0
+    while i < len(p_cards):
+        if not isJoker(p_cards[i]):
+            if p_cards[i][1] == suit:
+                return True
+        i += 1
+    return False
+
 
 def splitOnDifference(p_cards):
-	slice_start = 0
-	slice_end = 0
-	separate_cards = []
-	i = 0
-	joker_status = False
-	while i < (len(p_cards) - 1):
-		difference = ranks[p_cards[i + 1][0]] - ranks[p_cards[i][0]]
-		if difference == 2:
-			joker = getJoker(p_cards[i][1])
-			if joker != False:
-				p_cards.insert((i + 1), joker)
-				i += 1
-			else:
-				joker_status = True
-		if difference > 2 or joker_status:
-			slice_end = i + 1
-			temp = p_cards[slice_start:slice_end]
-			separate_cards.append(temp)
-			slice_start = i + 1
-		i += 1
-	
-	if slice_end < len(p_cards):
-		temp = p_cards[slice_end:]
-		separate_cards.append(temp)
-	
-	# Add joker if joker free and card length is 2
-	i = 0
-	while i < len(separate_cards):
-		if len(separate_cards[i]) == 2:
-			joker = getJoker(separate_cards[i][0][1])
-			if joker != False:
-				if ranks[separate_cards[i][0][0]] == ranks['ace']:
-					separate_cards[i].append(joker)
-				else:
-					separate_cards[i] = [joker] + separate_cards[i]
-		i += 1
-	return separate_cards
+    global deck
+    slice_start = 0
+    slice_end = 0
+    separate_cards = []
+    i = 0
+    joker_status = False
+    while i < (len(p_cards) - 1):
+        difference = ranks[p_cards[i + 1][0]] - ranks[p_cards[i][0]]
+        if difference == 2:
+            if deck != [] and checkIfSuitSame(p_cards, deck[1]):
+                if (ranks[p_cards[i][0]] + 1) == ranks[deck[0]]:
+                    p_cards.insert((i + 1), deck)
+                    i += 1
+                    deck = []
+                    continue
+            joker = getJoker(p_cards[i][1])
+            if joker != False:
+                p_cards.insert((i + 1), joker)
+                i += 1
+            else:
+                joker_status = True
+        if difference > 2 or joker_status:
+            slice_end = i + 1
+            temp = p_cards[slice_start:slice_end]
+            separate_cards.append(temp)
+            slice_start = i + 1
+        i += 1
+
+    if slice_end < len(p_cards):
+        temp = p_cards[slice_end:]
+        separate_cards.append(temp)
+
+    # Add joker if joker free and card length is 2
+    i = 0
+    while i < len(separate_cards):
+        if len(separate_cards[i]) == 2:
+            if deck != [] and checkIfSuitSame(separate_cards[i], deck[1]):
+                if ranks['ace'] == ranks[deck[0]]:
+                    if checkIfCardRankExists(separate_cards[i], ranks['king']) or (checkIfCardRankExists(separate_cards[i], ranks['joker']) and checkIfCardRankExists(separate_cards[i], ranks['queen'])):
+                        deck[0] = 'ace1'
+                        ranks['ace1'] = ranks['king'] + 1
+                        separate_cards[i].append(deck)
+                        deck = []
+                        continue
+                if (ranks[separate_cards[i][0][0]] - 1) == ranks[deck[0]]:
+                    separate_cards[i] = [deck] + separate_cards[i]
+                    deck = []
+                    continue
+                if (ranks[separate_cards[i][1][0]] + 1) == ranks[deck[0]]:
+                    separate_cards[i].append(deck)
+                    deck = []
+                    continue
+            joker = getJoker(separate_cards[i][0][1])
+            if joker != False:
+                if ranks[separate_cards[i][0][0]] == ranks['ace']:
+                    separate_cards[i].append(joker)
+                else:
+                    separate_cards[i] = [joker] + separate_cards[i]
+        i += 1
+    return separate_cards
+
 
 def sequenceCards(cards):
-	global jokers, new_cards
-	
-	i = 0
-	ace_counter = 1
-	while i < len(cards):
-		j = 0
-		new_cards.append([])
-		while j < len(cards[i]):
-			if isJoker(cards[i][j]):
-				jokers.append(cards[i][j])
-				if j == (len(cards[i]) - 1):
-					new_cards.remove([])
-			else:
-				if ranks[cards[i][j][0]] == ranks['ace']:
-					if checkIfCardRankExists(cards[i], ranks['king']) or (checkIfCardRankExists(cards[i], ranks['joker']) and checkIfCardRankExists(cards[i], ranks['queen'])):
-						cards[i][j][0] = 'ace' + str(ace_counter)
-						ranks['ace' + str(ace_counter)] = ranks['king'] + 1
-						ace_counter += 1
-				new_cards[i].append(cards[i][j])
-			j += 1
-		i += 1
+    global jokers, new_cards
 
-	copied_cards = deepcopy(new_cards)
-	i = 0
-	while i < len(copied_cards):
-		copied_cards[i] = sorted(copied_cards[i], key = lambda x: ranks[x[0]])
-		i += 1
+    i = 0
+    ace_counter = 1
+    while i < len(cards):
+        j = 0
+        new_cards.append([])
+        while j < len(cards[i]):
+            if isJoker(cards[i][j]):
+                jokers.append(cards[i][j])
+                if j == (len(cards[i]) - 1):
+                    new_cards.remove([])
+            else:
+                if ranks[cards[i][j][0]] == ranks['ace']:
+                    if checkIfCardRankExists(cards[i], ranks['king']) or (checkIfCardRankExists(cards[i], ranks['joker']) and checkIfCardRankExists(cards[i], ranks['queen'])):
+                        cards[i][j][0] = 'ace' + str(ace_counter)
+                        ranks['ace' + str(ace_counter)] = ranks['king'] + 1
+                        ace_counter += 1
+                new_cards[i].append(cards[i][j])
+            j += 1
+        i += 1
 
-	sequence_cards = deepcopy(copied_cards)
-	i = 0
-	while i < len(copied_cards):
-		temp = splitOnDifference(sequence_cards[0])
-		del sequence_cards[0]
-		sequence_cards.extend(temp)
-		i += 1
-	
-	return sequence_cards
+    copied_cards = deepcopy(new_cards)
+    i = 0
+    while i < len(copied_cards):
+        copied_cards[i] = sorted(copied_cards[i], key=lambda x: ranks[x[0]])
+        i += 1
 
-# hand_cards = [['joker', 'spades'], ['4', 'spades'], ['joker', 'spades'], ['2', 'spades'], ['8', 'clubs'], ['ace', 'spades'], ['joker', 'spades']]
-# hand_cards = [[['8', 'diamonds'], ['queen', 'diamonds']], [['4', 'clubs'], ['king', 'clubs'], ['ace', 'clubs']], [['8', 'hearts'], ['10', 'hearts'], ['5', 'hearts'], ['ace', 'hearts']], [['joker', 'spades'], ['8', 'spades'], ['queen', 'spades']], [['joker', None]]]
-# hand_cards = [[['8', 'diamonds'], ['10', 'diamonds']], [['4', 'clubs'], ['king', 'clubs'], ['ace', 'clubs']], [['8', 'hearts'], ['10', 'hearts'], ['5', 'hearts'], ['ace', 'hearts']], [['joker', 'spades'], ['8', 'spades'], ['queen', 'spades']], [['joker', None]]]
-hand_cards = [[['8', 'diamonds'], ['10', 'diamonds']], [['4', 'clubs'], ['king', 'clubs'], ['ace1', 'clubs']], [['8', 'hearts'], ['10', 'hearts'], ['5', 'hearts'], ['ace', 'hearts']], [['joker', 'spades'], ['8', 'spades'], ['queen', 'spades']], [['joker', None]]]
-# print()
-print(hand_cards)
-print()
-sorted_cards = sequenceCards(hand_cards)
+    sequence_cards = deepcopy(copied_cards)
+    i = 0
+    while i < len(copied_cards):
+        temp = splitOnDifference(sequence_cards[0])
+        del sequence_cards[0]
+        sequence_cards.extend(temp)
+        i += 1
 
-# sorted_cards = splitOnDifference(hand_cards[0])
-# print()
-print(sorted_cards)
-# print()
+    return sequence_cards
+
+
+def groupSingleCards(p_cards):
+    cards = []
+    single_cards = []
+    four_cards = True
+    for card in p_cards:
+        if len(card) == 1:
+            single_cards.append(card[0])
+        else:
+            if len(card) > 3 and four_cards:
+                cards.append(card[:4])
+                if len(card[4:]) == 1:
+                    single_cards.append(card[4:][0])
+                elif len(card[4:]) > 1:
+                    cards.append(card[4:])
+                four_cards = False
+            elif len(card) > 3:
+                cards.append(card)
+            else:
+                cards.append(card)
+    cards.append(single_cards)
+    return cards
+
+
+sequence_cards = sequenceCards(hand_cards)
+sequence_cards = groupSingleCards(sequence_cards)
